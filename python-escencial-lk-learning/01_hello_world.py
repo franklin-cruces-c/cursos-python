69ee22021e73b52f3a66ff5c25bf05fe7495b0b0
Franklin Cruces <150628856+franklin-cruces-c@users.noreply.github.com>
2026-03-13 12:30:05 +0000

    fea:conceptos basicos de python

---
+++ python-escencial-lk-learning/01_hello_world.py
@@ -0,0 +1,8 @@
+# Mi primer comentario
+print("Hello world") #Otro comentario
+#print("Otra impresion") #linea de codigo comentada
+
+
+
+'''Este es un comentario 
+multinea'''
---
+++ python-escencial-lk-learning/02_variables.py
@@ -0,0 +1,14 @@
+nombre = "Matusalen"
+
+edad = 1000
+
+estatura = 1.85
+
+positivo = True
+negativo = False
+
+
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
