3c7b747fd8feafea5e759554f662f07935839327
Franklin Cruces <150628856+franklin-cruces-c@users.noreply.github.com>
2026-03-23 11:24:54 +0000

    Feat: modulos y paquetes - importacion de modulos y paquetes

---
+++ python-escencial-lk-learning/06_modulos_paquetes.py
@@ -0,0 +1,9 @@
+#import datetime
+#import datetime as dt
+from datetime import datetime# as dt
+
+#hora_actual = datetime.datetime.now()
+#hora_actual = dt.datetime.now()
+hora_actual = datetime.now()
+
+print("Hora actual:",hora_actual)
